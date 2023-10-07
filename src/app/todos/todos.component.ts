f97d594a117f279ed0e234160d94e00ebd1579d7
Pranesh Palanisamy <ppalanisamy@presidio.com>
2023-10-07 12:24:25 +0530

    first commit

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss'],
+})
+export class TodosComponent {}
